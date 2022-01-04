04f66b7e50f3f033741f741250faada6d6d26b2c
Marcus <marcusventin@gmail.com>
2022-01-04 14:59:04 +0000

    Created a balance variable

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,12 @@
 require "oystercard"
 
 describe Oystercard do
+  describe "#initialize" do
+    it "has a default balance of 0" do
+      expect(subject.balance).to eq 0
+    end
+  end
+  
+  # it {expect(subject).to respond_to(:balance) }
+  
 end
